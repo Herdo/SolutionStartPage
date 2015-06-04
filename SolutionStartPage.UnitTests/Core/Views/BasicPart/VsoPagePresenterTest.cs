7ac19f9b48b517fc5bd0d9d80983023541fb353b
Stefan Over <herdosratos@googlemail.com>
2015-06-04 12:49:40 +0200

    Added unit tests for VsoPagePresenter

---
+++ SolutionStartPage.UnitTests/Core/Views/BasicPart/VsoPagePresenterTest.cs
@@ -0,0 +1,37 @@
+﻿namespace SolutionStartPage.UnitTests.Core.Views.BasicPart
+{
+    using System.Diagnostics.CodeAnalysis;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using SolutionStartPage.Core.Views.BasicPart;
+    using SolutionStartPage.Shared.Models;
+    using SolutionStartPage.Shared.Views.BasicPart;
+    using Telerik.JustMock;
+
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class VsoPagePresenterTest
+    {
+        [TestMethod]
+        public void Constructor()
+        {
+            // Arrange
+            var vsVersion = Mock.Create<IVisualStudioVersion>();
+            var ide = Mock.Create<IIde>();
+            var view = Mock.Create<IVsoPageView>();
+            var vm = Mock.Create<IVsoPageViewModel>();
+
+            var dataSourceConnected = false;
+            var startPageHeaderTitleSet = false;
+
+            Mock.Arrange(() => view.ConnectDataSource(vm)).DoInstead(() => dataSourceConnected = true);
+            Mock.ArrangeSet(() => vm.StartPageHeaderTitle = Arg.AnyString).DoInstead(() => startPageHeaderTitleSet = true);
+
+            // Act
+            var presenter = new VsoPagePresenter(vsVersion, ide, view, vm);
+
+            // Assert
+            Assert.IsTrue(dataSourceConnected);
+            Assert.IsTrue(startPageHeaderTitleSet);
+        }
+    }
+}
